5b5bfc56f7c572b3d75cff7ad2698dc9871a2ea8
surabhidas47 <137095550+surabhidas47@users.noreply.github.com>
2023-09-18 20:01:04 -0400

    Add files via upload

---
+++ nature_notes/src/main/java/com/journal/nature_notes/NatureNotesApplication.java
@@ -0,0 +1,13 @@
+package com.journal.nature_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatureNotesApplication.class, args);
+	}
+
+}
---
+++ nature_notes/src/test/java/com/journal/nature_notes/NatureNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journal.nature_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatureNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
